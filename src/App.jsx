01550e6456ad94039c6b13f9b5617e1106a9d028
Khairat Adesina <khairatadesina01@gmail.com>
2024-01-22 18:42:53 +0100

    add: called all components

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,18 @@ import Navbar from "../src/components/Navbar.jsx";
 import Intro from "../src/components/Intro.jsx";
 import background from "../src/assets/backgroundPattern.svg";
 import SocialProofSection from "../src/components/SocialProof.jsx";
-import ClearLinkAdvantage from "../src/components/ClearLinkAdvantage.jsx"
+import ClearLinkAdvantage from "../src/components/ClearLinkAdvantage.jsx";
+import TestimonialSection from "./components/Testimonial.jsx";
+import FaqSection from "./components/FAQ.jsx";
+import UseClearLink from "./components/UseClearLink.jsx";
+import FooterSection from "./components/Footer.jsx"
 
 function App() {
   const backgroundStyle = {
-    display: 'inline-flex',
+    display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
+    justifyContent: 'center',
     backgroundImage: `url(${background})`,
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover', // or 'contain', depending on your preference
@@ -22,6 +27,10 @@ function App() {
       </div>
       <SocialProofSection />
       <ClearLinkAdvantage />
+      <TestimonialSection />
+      <FaqSection />
+      <UseClearLink />
+      <FooterSection />
     </>
   )
 }
